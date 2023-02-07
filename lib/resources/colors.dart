a8c78d8873f720d17536b2eacdfd1726745aa3c0
Adit Agrawal <adit.agrawal100@gmail.com>
2023-02-08 04:18:13 +0530

    new

---
+++ lib/resources/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const operatorColor = Color(0xff4B5EFC);
+const buttonColorDark = Color.fromARGB(255, 25, 25, 25);
+const buttonColorLight = Colors.white;
+const primaryColor = Color.fromARGB(255, 250, 250, 250);
+const secondaryColor = Colors.black;
